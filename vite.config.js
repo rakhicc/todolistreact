5eb0a613b5da6033b66198af9a28431daabe2137
rakhicc <rakhicc29gmail.com>
2026-04-15 10:47:49 +0300

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,4 @@
 export default defineConfig({
   base: '/todolistreact/', // add this property and the value is the name of your repository
   plugins: [react()],
-  test: {
-    globals: true,
-    environment: 'jsdom',
-  } 
 })
